26a860217b68011dc75e06f32bec2bbda2823ce9
thegirlcoderr <lynndy2015@gmail.com>
2023-11-10 20:48:11 +0100

    added appId

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,14 @@ import { getFirestore } from 'firebase/firestore'
 
 
 // Your web app's Firebase configuration
+const REACT_APP_appId = "1: 83059426889: web: cbacfbe4db55886fde6198"
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_projectId,
     storageBucket: process.env.REACT_APP_storageBucket,
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.appId
+    appId: REACT_APP_appId
 };
 
 // Initialize Firebase
